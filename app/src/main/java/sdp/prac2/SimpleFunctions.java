58e604e4b32cd6fb20bba80d7d8ebc915b0241c8
MunasheMadz <142252418+g23m8754@users.noreply.github.com>
2025-04-11 08:36:28 +0200

    Task5 complete

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static boolean Task5(List<Integer> lst) {
+        if (lst.size() == 0) {
+            return true;
+        }
+        for (int i = 0; i < lst.size() - 1; i++) {
+            if (lst.get(i) > lst.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
