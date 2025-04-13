6dd4c9eab2c3e4e91783e30b0150b675fe2f881c
Ronewa <ronewamahuwa8@gmail.com>
2025-04-13 06:50:50 +0200

    Implement Task3 method for parenthesis matching

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,23 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    
+    public static boolean Task3(String s) {
+        int balance = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                balance++;
+            } else if (c == ')') {
+                balance--;
+                if (balance < 0) {
+                    return false;
+                }
+            }
+        }
+        return balance == 0; 
+    }
+
 }
